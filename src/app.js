6a57cb77544f1d595de65de782045904d859ee03
Nandhan-SwaaS <mnrsnandhanmanu@gmail.com>
2026-05-31 17:00:14 +0530

    Updated CORS

--- src/app.js
+++ src/app.js
@@ -101,7 +101,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173"
+    origin: [
+      "http://localhost:5173",
+      "https://city-issue-frontend.vercel.app"
+    ],
+    credentials: true
   })
 );
 
